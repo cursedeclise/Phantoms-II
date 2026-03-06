a8d0a79da940cd99769513d7330f3642791dc6fa
cursedeclise <167327316+cursedeclise@users.noreply.github.com>
2026-03-06 09:08:43 -0600

    prepared for projectiles to happen. added Globalvars.wait

--- Globalvars.gd
+++ Globalvars.gd
@@ -3,3 +3,7 @@ extends Node
 var score=0
 var playerhealth=120
 var iframes=true
+
+
+func wait(seconds: float) -> void:
+	await get_tree().create_timer(seconds).timeout
---
+++ global-objects/Birdleyhitbox.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.name == "Dart" and Globalvars.iframes==false:
+		Globalvars.playerhealth-=5
+		Globalvars.iframes=true
+		print("health: "+ str(Globalvars.playerhealth))
+		print("iframes on")
+		Globalvars.wait(1)
+		Globalvars.iframes=false
+		print("iframes off")
