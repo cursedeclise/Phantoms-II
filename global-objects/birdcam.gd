1943213caa07246afa0810d440d14f33e6f98b2d
cursedeclise <167327316+cursedeclise@users.noreply.github.com>
2026-04-10 09:28:47 -0500

    tuned camera

--- global-objects/birdcam.gd
+++ global-objects/birdcam.gd
@@ -2,4 +2,4 @@ extends Camera2D
 @export var Birdley: Node
 
 func _process(delta: float) -> void:
-	offset = offset.lerp((Globalvars.playerspeed/150)*Globalvars.direction, delta * 5)
+	offset = offset.lerp((Globalvars.playerspeed/180)*Globalvars.direction, delta * 1.5)
